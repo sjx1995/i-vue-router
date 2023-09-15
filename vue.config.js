4a9cad9980c802874b756984fea351a474b15cf3
sunly <sunjingxiang1995@gmail.com>
2023-09-15 11:05:57 +0800

    update: vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "/i-vue-router/",
+  transpileDependencies: true,
+});
